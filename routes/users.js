e064c929c11a0999c547526a7e0d76538678beb4
Ivy <ivyzhao335@gmail.com>
2025-11-21 23:43:31 -0500

    update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const Emojis = require('../models/emojis')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/all', async function(req, res, next) {
+  try{
+    const emojis = (await Emojis.find()).toSorted({
+      createdAt: -1
+    })
+    res.json(emojis)
+  } catch (error){
+    next(error)
+  }
 });
 
 module.exports = router;
