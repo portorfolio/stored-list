f3ec2d6bcd17b464c423a2185700ac596b8d4605
Ivy <ivyzhao335@gmail.com>
2025-11-27 18:44:23 -0500

    Update emojis.js

--- routes/emojis.js
+++ routes/emojis.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 const Emojis = require('../models/emojis')
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
-});
-
 router.get('/all', async function (req, res) {
   try {
     const emojis = await Emojis.find()
