5fd9b01f3ca1495a0bbe30d5b42255b932a76b85
Ivy <ivyzhao335@gmail.com>
2025-11-29 22:53:57 -0500

    update

---
+++ public/javascripts/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const popupOverlay = document.getElementById('popup');
+    const closePopupButton = document.getElementById('close-popup');
+
+    // Show the popup when the page loads
+    popupOverlay.style.display = 'flex'; // Use flex to center content
+
+    // Hide the popup when the close button is clicked
+    closePopupButton.addEventListener('click', function () {
+        popupOverlay.style.display = 'none';
+    });
+});
